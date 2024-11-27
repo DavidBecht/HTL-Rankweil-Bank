4071b57e5e9642cb8fb1dd604b6f22ef56c075e4
thomas.schallert@gmx.at <thomas.schallert@gmx.at>
2024-11-27 10:51:34 +0000

    Created Form2 and Backend; Edited settings and Form1

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Form2Template
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,13 +6,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class Form2(Form2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def outlined_button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
