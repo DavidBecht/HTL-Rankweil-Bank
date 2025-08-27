7e1fb4663691f8f716659b9cc162f44df8600a50
david.bechtold@htl-rankweil.at <david.bechtold@htl-rankweil.at>
2025-08-27 08:08:42 +0000

    Edited Login, Backend, Users and Startpage

--- client_code/Startpage/__init__.py
+++ client_code/Startpage/__init__.py
@@ -18,29 +18,23 @@ class Startpage(StartpageTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
-    url = anvil.js.window.location.href
-    queryparams = anvil.server.call('get_query_params', url)
-    state = anvil.server.call('get_login_state')
-    if not state and queryparams.get('AccountNo', [None])[0] != None:
-      Resultpage = open_form('Resultpage', 'x','x', self.check_box_1.checked, "nologin")
-      Resultpage.Label_result.text = "Not Logged in"
-    if state is True:
-      open_form('Resultpage', '','', self.check_box_1.checked)
-    else:
-      self.logout_clean_url()
-
 
   def login_button_click(self, **event_args):
     username = self.textbox_username.text
-    passwort = self.textbox_passwort.text
-
-    # anvil.server.call('set_session_secureinput',secureinput)
-    # anvil.server.call('set_session_accno', anvil.server.call('get_acc_no', username, passwort))
-    # self.Label_result.text =  anvil.server.call("get_user",username, passwort, url, secureinput)
-    # open_form('Resultpage',username,passwort, secureinput)
-    res, msg, user =  anvil.server.call("get_user_simple",username, passwort)
+    password = self.textbox_passwort.text
+    res, msg, user =  anvil.server.call("get_user_simple",username, password)
+    query = f"SELECT username FROM Users WHERE username = '{username}'"
+    res, exc, _user = anvil.server.call("execute_query_raw", query)
+    query = f"SELECT password FROM Users WHERE password = '{password}'"
+    res, exc, _password = anvil.server.call("execute_query_raw", query)
+    print(_user)
+    print(_password)
     if not res or not user or user.get('username', None) == None:
       self.textarea_error.text = msg
+    elif _user is not None and len(_user) > 0 and username == _user[0] and _password is not None and len(_password) > 0 and password == _password[0]:
+      query = f"SELECT accountno FROM Users WHERE username = '{username}'"
+      res, exc, _accno = anvil.server.call("execute_query_raw", query)
+      routing.set_url_hash(url_pattern="users", url_dict={'username': _user[0], 'accountno': _accno[0]})
     else:
       routing.set_url_hash(url_pattern='login', url_dict={'username': self.textbox_username.text})
       
